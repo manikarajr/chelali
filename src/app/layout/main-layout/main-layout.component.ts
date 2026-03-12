05a6d37b69f411ab6033fab02e43be3466f8b2b6
devika <coder@ckssolutions.co.in>
2026-03-12 14:45:47 +0530

    bottombar

--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -1,14 +1,16 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { SidebarComponent } from '../sidebar/sidebar.component';
+import { NAV_ITEMS } from '../sidebar/sidebar.config';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, SidebarComponent],
   templateUrl: './main-layout.component.html',
 })
 export class MainLayoutComponent {
   sidebarOpen = signal(false);
   toggleSidebar() { this.sidebarOpen.update(v => !v); }
+  navItems = NAV_ITEMS;
 }
