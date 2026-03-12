e005f50cf2c6428b7e94831c404d1b0ad902348c
devika <coder@ckssolutions.co.in>
2026-03-12 14:41:45 +0530

    sidebar changes

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,6 +1,5 @@
 import { Component, computed, input, output } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-
 import { NAV_ITEMS } from './sidebar.config';
 
 @Component({
@@ -14,13 +13,10 @@ export class SidebarComponent {
   isOpen = input(false);
   closed = output<void>();
 
-  sidebarClasses = computed(() =>
-    'fixed bg-gray-900 text-white flex flex-col z-50 will-change-transform ' +
-    // Mobile: bottom bar
-    'bottom-0 left-0 right-0 h-16 border-t border-gray-800 ' +
-    // Desktop: left sidebar
-    'lg:top-0 lg:bottom-0 lg:left-0 lg:right-auto lg:h-full lg:w-64 lg:border-t-0'
-  );
+  sidebarClasses = computed(() => [
+    'fixed top-0 left-0 h-full w-64 bg-gray-900 text-white flex flex-col z-50 transition-transform duration-300 ease-in-out',
+    this.isOpen() ? 'translate-x-0' : '-translate-x-full lg:translate-x-0',
+  ].join(' '));
 
   navItems = NAV_ITEMS;
 }
